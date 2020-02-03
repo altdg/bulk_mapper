337da01eb894538b7f63397aaaa9b92e431a81e2
Alex <alexg@altdg.com>
2020-02-03 11:07:03 +0300

    Add test

---
+++ test_mapper.py
@@ -0,0 +1,26 @@
+import re
+import subprocess
+from os import getenv, path, listdir
+
+
+def test_cli_tool_on_live_api():
+    """ Checks whether the tool may be invoked using CLI and the resulting CSV file is generated """
+
+    api_key = getenv('ADG_API_KEY')
+    if not api_key:
+        raise ValueError('ADG_API_KEY env var not set')
+
+    pth = path.dirname(path.abspath(__file__))
+
+    subprocess.run(['rm', path.join(pth, 'sample-merchants-*.csv')])
+
+    result = subprocess.run([
+        'python', path.join(pth, 'adg_api_bulkMapper.py'),
+        '-e', 'merchants',
+        '-k', api_key,
+        path.join(pth, 'sample-merchants.txt'),
+    ])
+    result.check_returncode()
+
+    assert any(re.match(r'sample-merchants-.*\.csv', name) for name in listdir(pth)), \
+        'CSV file not generated in target folder'
